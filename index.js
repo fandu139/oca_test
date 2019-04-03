91889f5aa0e719578c2e72430ef35bb9bb6cd7eb
fandu139 <fandupratama@gmail.com>
2019-04-03 22:48:28 +0800

    second commit

--- index.js
+++ index.js
@@ -6,123 +6,57 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-/*var objparkir = {
-   tablemaxparkir: []
-};
-
-objparkir.tablemaxparkir.push({max: 10});
-
-var jsonparkir = JSON.stringify(objparkir);*/
 
 var maxtempatparkir = 10;
 
 var obj = {
-   table: [],
+   //table: [],
    tabletempatparkir: [],
-   tabledetailparkir: []
+   //tabledetailparkir: []
 };
-
-obj.table.push({id: 1, square:2});
-//obj.tabletempatparkir.push({max: 10});
 
 var json = JSON.stringify(obj);
 
-
 var fs = require('fs');
 fs.writeFile('myjsonfile.json', json, 'utf8');
-//fs.writeFile('myjsonfile.json', jsonparkir, 'utf8');
 
-fs.readFile('myjsonfile.json', 'utf8', function readFileCallback(err, data){
+/*fs.readFile('myjsonfile.json', 'utf8', function readFileCallback(err, data){
     if (err){
         console.log(err);
     } else {
-		obj = JSON.parse(data); //now it an object
-		obj.table.push({id: 2, square:3}); //add some data
-		for(var a = 1; a <= maxtempatparkir; a++){
-			obj.tabletempatparkir.push({slotparkir: 'A'+a, platmobil: ''});
-		}
-		json = JSON.stringify(obj); //convert it back to json
-		fs.writeFile('myjsonfile.json', json, 'utf8'); // write it back 
-	}
-});
+  		obj = JSON.parse(data); //now it an object
+  		for(var a = 1; a <= maxtempatparkir; a++){
+  		  obj.tabletempatparkir.push({slotparkir: 'A'+a, tabledetailparkir: ''});
+        //obj.tabletempatparkir.push({'A': []});
+  		}
+  		json = JSON.stringify(obj); //convert it back to json
+  		fs.writeFile('myjsonfile.json', json, 'utf8'); // write it back
+	  }
+});*/
 
 
 app.get('/persons/:username', function(req, res){
     var param_username = req.params.username;
-	res.send(param_username);
-    
+	  res.send(param_username);
 });
 
 app.get("/", function(req, resp){
-    resp.send("masuk");
-	var path = 'myjsonfile.json';
-	const loadData = (path) => {
-	  try {
-		return fs.readFileSync(path, json 'utf8')
-	  } catch (err) {
-		console.error(err)
-		return false
-	  }
-	}
+    var tes = fs.readFileSync('myjsonfile.json', 'utf8');
+    var json = JSON.parse(tes);
+    resp.json(json);
+    console.log(json)
+	   /*const loadData = (path) => {
+  	   try {
+  		    return fs.readFileSync(path, json 'utf8')
+  	   } catch (err) {
+  		    console.error(err)
+  		    return false
+  	   }
+	   }*/
 });
 
-var fibonacci = [];
-
-app.get("/soal1", function(req, resp){
-
-  var xn = 0;
-  var a = 0;
-  var b = 1;
-  var limit = 10
-
-  for(z=0; z<limit; z++){
-    xn = a + b ;
-    view(xn);
-    a = b;
-    b = xn;
-  }
-
-  resp.send(fibonacci);
-
-});
-
-function view(xn) {
-  fibonacci.push(xn);
-  console.log("masuk fandu = "+xn);
-}
-
-app.get("/soal2", function(req, resp){
-
-  var a = [1, 2, 0, 5, 8, 1, 3];
-  var b = [3, 0, 7, 5, 1, 9];
-
-  var benar = 0;
-  var salah = 0;
-
-  b.forEach(function(item, index, array){
-    var status = false;
-    a.forEach(function(item_a, index_a, array_a){
-      if ( item === item_a){
-        status = true;
-      }
-    });
-    console.log("soal nomor 2 = apakah tabel B yaitu "+item+" subarray dari tabel A = "+status);
-
-    if (status){
-      benar++;
-    } else {
-      salah++;
-    }
-
-  });
-
-  resp.send("Jumlah Array Benar "+ benar +" dan Salah "+salah);
-
-});
-
-var soal3 = require('./router/soal3');
-app.use('/soal3', soal3);
-
+var parkir = require('./router/parkir');
+app.use('/parkir', parkir);
 
 app.listen(1337, function(){
   console.log("Server sudah jalan di port 1337");
---
+++ router/parkir.js
@@ -0,0 +1,96 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+
+router.use(bodyParser.json());
+
+// http://localhost:1337/parkir/get_parkir/D101
+router.get('/get_parkir/:platmobil', function(req, resp, next) {
+
+	var plat = req.params.platmobil;
+
+	fs.readFile('myjsonfile.json', 'utf8', function readFileCallback(err, data){
+			if (err){
+					console.log(err);
+			} else {
+				obj = JSON.parse(data); //now it an object
+
+				var res = {};
+
+				obj.tabletempatparkir.forEach(function(item, index, array){
+						if (item.platmobil === plat){
+
+							var biayaparkir = 1;
+							var masuk = item.tanggal_masuk;
+
+							const date = new Date();
+							var keluar = date.getFullYear()+'-'+date.getMonth()+'-'+date.getDate()+' '+date.getHours()+':'+date.getMinutes();
+
+							var selisih = Math.floor((new Date(keluar) - new Date(masuk)) / 1000 / 60 / 60);
+
+							if (item.tipe === "SUV"){
+									biayaparkir = 25000;
+							} else if (item.tipe === "MPV"){
+									biayaparkir = 35000
+							}
+
+							var biaya = biayaparkir;
+							if (selisih > 1){
+								biaya = ((selisih - 1) * (biayaparkir * 20 /100)) + biayaparkir;
+							} else {
+								biaya = biayaparkir;
+							}
+
+
+							res = {
+								platmobil : item.platmobil,
+								tanggal_masuk : masuk,
+								tanggal_keluar : keluar,
+								total : biaya
+							}
+						}
+
+				});
+				resp.send(res);
+
+			}
+	});
+});
+
+router.post('/post_parkir', function(req, resp) {
+    var plat = req.body.plat;
+    var warna = req.body.warna;
+    var tipe = req.body.tipe;
+
+		fs.readFile('myjsonfile.json', 'utf8', function readFileCallback(err, data){
+				if (err){
+						console.log(err);
+				} else {
+					obj = JSON.parse(data); //now it an object
+
+					const date = new Date();
+
+					var a = obj.tabletempatparkir.length + 1;
+					var res = {
+						ruangparkir : 'A'+a,
+						platmobil : plat,
+						warna : warna,
+						tipe : tipe,
+						tanggal_masuk : date.getFullYear()+'-'+date.getMonth()+'-'+date.getDate()+' '+date.getHours()+':'+date.getMinutes()
+					};
+
+					//obj.tabletempatparkir.push({ id: a , parkir: [res]});
+					obj.tabletempatparkir.push(res);
+
+		  		json = JSON.stringify(obj); //convert it back to json
+
+		  		fs.writeFile('myjsonfile.json', json, 'utf8'); // write it back
+
+					resp.send(res);
+
+				}
+		});
+});
+
+module.exports = router;
